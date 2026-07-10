cc6dcb9e999e63ec37e5b5d4f149763745ab8821
Panagiotis <ppanagiotisilv@gmail.com>
2026-07-11 01:07:23 +0300

    astro fonts

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,31 @@
-// @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig, fontProviders } from 'astro/config';
+import purgeCSSPlugin from '@fullhuman/postcss-purgecss';
+const isProduction = process.env.NODE_ENV === 'production';
 
-// https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  prefetch: true,
+  fonts: [
+    {
+      name: "Cormorant Garamond",
+      cssVariable: "--cormorant",
+      provider: fontProviders.google(),
+      weights: [400, 500, 600]
+    }
+  ],
+  vite: {
+    css: {
+      preprocessorOptions: {
+        scss: {
+          quietDeps: true
+        }
+      },
+      postcss: {
+        plugins: isProduction ? [
+          purgeCSSPlugin({
+            content: ['./src/**/*.astro']
+          })
+        ] : []
+      }
+    }
+  }
+});
