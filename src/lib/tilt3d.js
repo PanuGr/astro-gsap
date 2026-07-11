6a43c088cbb25bffbdad755763e138ed0374c261
Panagiotis <ppanagiotisilv@gmail.com>
2026-07-12 02:15:46 +0300

    fix

--- src/lib/tilt3d.js
+++ src/lib/tilt3d.js
@@ -9,26 +9,22 @@ function initCardTilt() {
     cards.forEach(card => {
       gsap.set(card, { transformPerspective: 800 });
 
+      const setRotX = gsap.quickTo(card, 'rotationX', { duration: 0.3, ease: 'power2.out' });
+      const setRotY = gsap.quickTo(card, 'rotationY', { duration: 0.3, ease: 'power2.out' });
+
       card.addEventListener('mousemove', e => {
         const rect = card.getBoundingClientRect();
         const x = e.clientX - rect.left;
         const y = e.clientY - rect.top;
         const hw = rect.width / 2;
         const hh = rect.height / 2;
-        gsap.set(card, {
-          rotationX: ((y - hh) / hh) * -6,
-          rotationY: ((x - hw) / hw) * 6,
-        });
+        setRotX(((y - hh) / hh) * -6);
+        setRotY(((x - hw) / hw) * 6);
       });
 
       card.addEventListener('mouseleave', () => {
-        gsap.to(card, {
-          rotationX: 0,
-          rotationY: 0,
-          duration: 0.3,
-          ease: 'power2.out',
-          overwrite: 'auto',
-        });
+        setRotX(0);
+        setRotY(0);
       });
     });
   });
